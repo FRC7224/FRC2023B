430c10f14e6ac4e4bcfcb2e0d30b334c692138ef
Marty Gubow <martygubow@gmail.com>
2023-01-17 18:57:36 -0500

    New lib

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,98 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+/*
+ * Initially from https://github.com/Mechanical-Advantage/RobotCode2022
+ */
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.RobotBase;
+import frc.lib.team6328.util.Alert;
+import frc.lib.team6328.util.Alert.AlertType;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+
+  public static final double LOOP_PERIOD_SECS = 0.02;
+
+  public static final boolean TUNING_MODE = false;
+
+  // An empty string uses the default CAN bus; specify the name of the CANivore as
+  // appropriate
+  public static final String CAN_BUS_NAME = "";
+
+  // FIXME: specify the name of the camera used for detecting AprilTags
+  public static final String CAMERA_NAME = "Front";
+
+  private static final RobotType ROBOT = RobotType.ROBOT_2023_SEASON;
+
+  private static final Alert invalidRobotAlert =
+      new Alert("Invalid robot selected, using competition robot as default.", AlertType.ERROR);
+
+  public static RobotType getRobot() {
+    if (RobotBase.isReal()) {
+      if (ROBOT == RobotType.ROBOT_SIMBOT) { // Invalid robot selected
+        invalidRobotAlert.set(true);
+        return RobotType.ROBOT_2023_SEASON;
+      } else {
+        return ROBOT;
+      }
+    } else {
+      return ROBOT;
+    }
+  }
+
+  // update for various robots
+  public static Mode getMode() {
+    switch (getRobot()) {
+      case ROBOT_2023_SEASON:
+        return RobotBase.isReal() ? Mode.REAL : Mode.REPLAY;
+
+      case ROBOT_SIMBOT:
+        return Mode.SIM;
+
+      default:
+        return Mode.REAL;
+    }
+  }
+
+  public enum RobotType {
+    ROBOT_2023_SEASON,
+    ROBOT_SIMBOT
+  }
+
+  public enum Mode {
+    REAL,
+    REPLAY,
+    SIM
+  }
+
+  // Joystick 1 constants
+  public static double kdeadzone = 0.05; // Deadzone
+
+  public static int kshiftbutton = 1;
+  public static int kintakeupbutton = 2;
+  public static int kdrvautobutton = 3;
+  public static int kclimboverridebutton = 4;
+
+  public static int kshoot5button = 5;
+  public static int kshoot6button = 6;
+  // public static int kshortshootbutton = 7;
+  public static int kinitShooter = 8;
+  public static int kautobutton = 9;
+  public static int kclimbButton = 10;
+  public static int kclimbButtonInital = 7;
+  public static int kshoot0button = 11;
+  public static int kshoot7button = 12;
+
+  public static int ARM_EXTEND_PORT = 14;
+}
